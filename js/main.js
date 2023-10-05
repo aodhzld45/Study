45da09388ab1e088e412d71151e8f22221e8fbc7
hyunseok <prking94@naver.com>
2023-10-05 16:00:42 +0900

    2023-10-05 Js npm 초기설정 및 초기화 작업

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-var username = document.querySelector("#username");
-var userpassword = document.querySelector("#userpassword");
+import _ from "lodash";
+// var username = document.querySelector("#username");
+// var userpassword = document.querySelector("#userpassword");
 
-var register = document.querySelector("#register")
+// var register = document.querySelector("#register")
 
-register.onclick = function name(params) {
-    alert("제출 버튼 클릭시 발생하는 이벤트.");
-    alert(username.value);
-    alert(userpassword.value);
-}
+// register.onclick = function name(params) {
+//     alert("제출 버튼 클릭시 발생하는 이벤트.");
+//     alert(username.value);
+//     alert(userpassword.value);
+// }
+
+const els = document.querySelectorAll('div')
+els.forEach(el => {
+    console.log(el.dataset.fruitName)
+})
+
+console.log("parcel test");
+console.log(_.camelCase("camel test"));
 
 
